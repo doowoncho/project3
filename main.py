21d1a9b00cbe0b388d7a0ab74d86a81a9290b7e9
doowoncho <84430881+doowoncho@users.noreply.github.com>
2022-06-08 15:28:20 +0000

    second iteration

--- main.py
+++ main.py
@@ -7,21 +7,41 @@ turtle.shape("turtle")
 screen = turtle.Screen()
 sally = turtle.Turtle()
 
-def star():
-  print("draw star!")
+def star(color, length):
+
+  sally.fillcolor(color)
+  sally.begin_fill()
+
+  corners = 0
+  while corners < 5:
+    sally.forward(length)
+    sally.right(144)
+    corners += 1
+
+  sally.end_fill()
+
 
 def triangle():
-  print("draw triangle!")
+  sally.fillcolor(color)
+  sally.begin_fill()
 
+  corners = 0
+  while corners < 3:
+    sally.forward(length)
+    sally.right(120)
+    corners += 1
+
+  sally.end_fill()
+  
 def circle():
   print("draw circle!")
 
 shape = input("give me a shape: ")
-# color = input("give me a color: ")
-# length = input("give me a length: ")
+color = input("give me a color: ")
+length = int(input("give me a length: "))
 
 if shape == "star":
-  star()
+  star(color, length)
 elif shape =="triangle":
   triangle()
 elif shape == "circle":
